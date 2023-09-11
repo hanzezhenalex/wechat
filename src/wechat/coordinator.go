e2fa6639e5835208781202b8f908bf3283fd650f
I548435 <alex.han@sap.com>
2023-09-11 22:32:09 +0800

    fix: do not close

--- src/wechat/coordinator.go
+++ src/wechat/coordinator.go
@@ -2,7 +2,6 @@ package wechat
 
 import (
 	"encoding/xml"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/hanzezhenalex/wechat/src"
@@ -25,22 +24,13 @@ func NewCoordinator(cfg src.Config) *coordinator {
 
 func (c *coordinator) Handler() gin.HandlerFunc {
 	return func(context *gin.Context) {
-		raw, err := ioutil.ReadAll(context.Request.Body)
-		if err != nil {
-			cTracer.Errorf("fail to read request body, %s", err.Error())
-			context.Writer.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		_ = context.Request.Body.Close()
-
-		cTracer.Debugf("recive a msg from wechat, msg=%s", string(raw))
-
 		var msg message
 		if err := xml.NewDecoder(context.Request.Body).Decode(&msg); err != nil {
 			cTracer.Errorf("fail to decode request body, %s", err.Error())
 			context.Writer.WriteHeader(http.StatusBadRequest)
 			return
 		}
+		_ = context.Request.Body.Close()
 
 		_, _ = context.Writer.WriteString("")
 	}
