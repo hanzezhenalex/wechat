496d8caa5881b3b8f92c01ee97544d98dcefc991
I548435 <alex.han@sap.com>
2023-09-11 22:54:51 +0800

    fix: msg type

--- src/wechat/coordinator.go
+++ src/wechat/coordinator.go
@@ -24,7 +24,7 @@ func NewCoordinator(cfg src.Config) *coordinator {
 
 func (c *coordinator) Handler() gin.HandlerFunc {
 	return func(context *gin.Context) {
-		var msg message
+		var msg Message
 		if err := xml.NewDecoder(context.Request.Body).Decode(&msg); err != nil {
 			cTracer.Errorf("fail to decode request body, %s", err.Error())
 			context.Writer.WriteHeader(http.StatusBadRequest)
@@ -32,17 +32,28 @@ func (c *coordinator) Handler() gin.HandlerFunc {
 		}
 		_ = context.Request.Body.Close()
 
-		_, _ = context.Writer.WriteString("")
+		var ret string
+
+		switch msg.MsgType {
+		case msgText:
+			ret = ""
+		default:
+			ret = "不支持当前消息类型"
+		}
+		_, _ = context.Writer.WriteString(ret)
 	}
 }
 
-// use struct + inline type?
-type message map[string]interface{}
-
-func (m message) fromUsername() string {
-	return m["FromUsername"].(string)
+type Message struct {
+	ToUserName   string                 `xml:"ToUserName"`
+	FromUserName string                 `xml:"FromUserName"`
+	CreateTime   string                 `xml:"CreateTime"`
+	MsgType      string                 `xml:"MsgType"`
+	MsgId        string                 `xml:"MsgId,omitempty"`
+	Content      string                 `xml:"Content"`
+	Others       map[string]interface{} `xml:",innerxml"`
 }
 
-func (m message) msgType() string {
-	return m["MsgType"].(string)
-}
+const (
+	msgText = "text"
+)
