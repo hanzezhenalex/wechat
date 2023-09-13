55b958d4d7d03fbbf1ee1891d9208c0fbb9b0eba
I548435 <alex.han@sap.com>
2023-09-13 14:32:36 +0800

    fix: update scripts

---
+++ deploy/deploy_wechat_server.sh
@@ -0,0 +1,34 @@
+set -exo pipefail
+
+DOCKER_WECHAT_SERVER="wechat"
+PRODUCTION_MODE=$1
+
+echo "clean up wechat_server"
+
+containers=$(docker ps | grep "${DOCKER_WECHAT_SERVER}" | awk '{print 1}')
+
+if [ -z "${containers}" ]; then
+  echo "no running containers "
+else
+  docker container stop "${containers}"
+fi
+
+containers=$(docker ps -a | grep "${DOCKER_WECHAT_SERVER}" | awk '{print 1}')
+
+if [ -z "${containers}" ]; then
+  echo "no stopped containers"
+else
+  docker container stop "${containers}"
+fi
+
+make docker_wechat_server
+
+if [ -z "${PRODUCTION_MODE}" ]; then
+  # debug mode
+  docker run -d --rm --name "${DOCKER_WECHAT_SERVER}" -p 8096:8096 alex/wechat_server:latest
+else
+  docker run -d --name "${DOCKER_WECHAT_SERVER}" -p 8096:8096 \
+      --network "${DOCKER_NETWORK}" \
+      -v "${CONFIG_FILE_FOLDER}":/usr/app/wechat \
+      alex/wechat_server:latest --config "/usr/app/wechat/${CONFIG_FILE_NAME}"
+fi
